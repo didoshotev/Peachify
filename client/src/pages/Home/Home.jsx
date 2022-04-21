5261860d9f95db474ddeacc906fb6a49c7cd014d
didoshotev <didoshotev@gmail.com>
2022-04-21 20:31:28 +0300

    updates

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,31 +1,21 @@
-import { Button } from '@mui/material';
+import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import classes from './Home.module.scss';
 
 function Home() {
     return (
-        <div className="page-bg">
+        <div className={classes['home-page-bg']}>
             <section className={classes.content}>
-                <div style={{ paddingBottom: '100px', paddingTop: '150px' }}>
-                    <img src="/icons/peachify-logo-white-png.png" width={250} height={100} alt="peachify-logo" />
+                <div style={{ paddingBottom: '90px', paddingTop: '100px' }}>
+                    <img src="/icons/peachify-logo-white-png.png" width={300} height={100} alt="peachify-logo" />
                 </div>
-                <Link to="/dashboard">
-                    <Button
-                        className={classes['content-action']}
-                        sx={{
-                            border: '3px solid #FFA200',
-                            height: '70px',
-                            width: '400px',
-                            borderRadius: '39px',
-                            backgroundColor: 'rgb(68, 37, 79)',
-                            fontFamily: 'MoonLight',
-                            fontSize: '25px',
-                        }}
-                        variant="contained"
-                    >
-                        Connect Wallet
-                    </Button>
-                </Link>
+                <Box className={classes.btn} type="button">
+                    <Link to="/dashboard">
+                        <span className={classes['btn-content']}>
+                            Connect Wallet
+                        </span>
+                    </Link>
+                </Box>
                 <div style={{ marginTop: 100 }}>
                     <img
                         src="/icons/discord-white.svg"
